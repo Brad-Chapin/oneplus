63c47eb37ebfec25b790c4df1ff3b9bd2c7b45db
Brad Chapin <paledoor@yahoo.com>
2017-01-16 09:43:58 -0700

    added empty array test, moved test file to test folder

--- test/test.js
+++ test/test.js
@@ -1,9 +1,12 @@
 const assert = require('assert');
-const main = require ("./main.js");
+const main = require ("../main.js");
 
 
 describe ("onePlus", function(){
   it("should add 1 to each array element, then return their sums", function (){
     assert.equal(main.onePlus([1,2,3,4]), 14);
+  });
+  it("should return 0 if the array is empty", function (){
+    assert.equal(main.onePlus([]), 0);
   });
 });
